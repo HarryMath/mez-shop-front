76ad2f0d42fd0fb1272178e62af7312ef2fd4ad2
RedmiBook <nikitabort22092000@gmail.com>
2021-04-01 18:24:01 +0300

    mobile version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { ContentComponent } from './content/content.component';
 import { EngineComponent } from './engine/engine.component';
 import { FormsModule } from '@angular/forms';
 import { CatalogComponent } from './catalog/catalog.component';
@@ -25,7 +24,6 @@ const appRoutes = [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ContentComponent,
     EngineComponent,
     CatalogComponent,
     HomePageComponent,
