03999978966c8cb24fe372698a790b3594464efb
RedmiBook <nikitabort22092000@gmail.com>
2021-04-06 12:08:46 +0300

    google front auth

--- src/app/catalog-page/catalog-page.component.ts
+++ src/app/catalog-page/catalog-page.component.ts
@@ -1,12 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import {MenuService} from '../shared/menu.service';
 
+export interface FilterBlock {
+  name: string;
+  opened: boolean;
+  options: FilterOption[];
+}
+
+export interface FilterOption {
+  name: string;
+  selected: boolean;
+}
+
+
 @Component({
   selector: 'app-catalog-page',
   templateUrl: './catalog-page.component.html',
   styleUrls: ['./catalog-page.component.css']
 })
 export class CatalogPageComponent implements OnInit {
+
+  filters: FilterBlock[] = [
+    {name: 'тип', opened: true, options: [
+        {name: 'Стандартные двигатели', selected: false},
+        {name: 'Двигатели для нефтегазовой промышленности', selected: false},
+        {name: 'Двигатели для объектов использования ядерного топлива', selected: false},
+        {name: 'Многоскоростные двигатели', selected: false},
+        {name: 'Двигатели с электромагнитным тормозом', selected: false},
+        {name: 'Двигатели с повышенным скольжениемм', selected: false},
+        {name: 'Тяговые двигатели', selected: false},
+        {name: 'Двигатели с повышенным скольжениемм', selected: false},
+        {name: 'Узкоспециализированные двигатели', selected: false},
+        {name: 'Встраиваемые двигатели', selected: false},
+        {name: 'Бытовые однофазные двигатели типа ДАК', selected: false},
+        {name: 'Товары народного потребления', selected: false},
+        {name: 'Электроакустические приборы', selected: false},
+      ]},
+    {name: 'производитель', opened: true, options: [
+        {name: 'производитель 1', selected: false},
+        {name: 'производитель 2', selected: false},
+        {name: 'производитель 3', selected: false},
+      ]}
+  ];
 
   constructor(public menuService: MenuService) { }
 
--- src/assets/googleAuth.js
+++ src/assets/googleAuth.js
@@ -4,9 +4,26 @@ function handleGoogleLoad() {
 function showAuthWindow() {
   const clientId = '475065897388-gq0ldd5fgc6o3bhbvunnj3piur1psteq.apps.googleusercontent.com'
   google.accounts.id.initialize({
-    client_id: clientId
+    client_id: clientId,
+    callback: onGoogleSignedIn
   })
   google.accounts.id.prompt(notification => {
     console.log(notification)
   })
+}
+
+function onGoogleSignedIn(response) {
+  const clientId = response.clientId
+  const info = parseJwt(response.credential)
+  const photo = info.picture
+  const name = info.name
+}
+
+function parseJwt (token) {
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+  return JSON.parse(jsonPayload);
 }
