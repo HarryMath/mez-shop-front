a5f766b8f2f775b4004caca1e8d50a84f0ebc194
RedmiBook <nikitabort22092000@gmail.com>
2021-04-15 20:07:33 +0300

    engine template changed for current database structure

---
+++ src/app/components/department/department.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Department} from '../../pages/contacts-page/contacts-page.component';
+
+
+@Component({
+  selector: 'app-department',
+  templateUrl: './department.component.html',
+  styleUrls: ['./department.component.css']
+})
+export class DepartmentComponent implements OnInit {
+
+  @Input() department!: Department;
+  opened = false;
+
+  constructor() { }
+
+  getStateString(): string {
+    return this.opened ? 'свернуть' : 'раскрыть';
+  }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/components/engine/engine.component.ts
+++ src/app/components/engine/engine.component.ts
@@ -14,4 +14,30 @@ export class EngineComponent implements OnInit {
 
   ngOnInit(): void { }
 
+  getPower(): string {
+    let result = '';
+    for (let i = 0; i < this.engine.characteristics.length; i++) {
+      result += this.engine.characteristics[i].power;
+      if (i !== this.engine.characteristics.length - 1) { result += '/'; }
+    }
+    return result;
+  }
+
+  getFrequency(): string {
+    let result = '';
+    for (let i = 0; i < this.engine.characteristics.length; i++) {
+      result += this.engine.characteristics[i].frequency;
+      if (i !== this.engine.characteristics.length - 1) { result += '/'; }
+    }
+    return result;
+  }
+
+  getEfficiency(): string {
+    let result = '';
+    for (let i = 0; i < this.engine.characteristics.length; i++) {
+      result += this.engine.characteristics[i].efficiency;
+      if (i !== this.engine.characteristics.length - 1) { result += '/'; }
+    }
+    return result;
+  }
 }
